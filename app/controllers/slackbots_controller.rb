d946d6dc288a11f4f0d25ebb67201428b1bc29d3
坪内崇人 <taka.ur.0505@gmail.com>
2020-02-02 20:02:25 +0900

    インデント修正

--- app/controllers/slackbots_controller.rb
+++ app/controllers/slackbots_controller.rb
@@ -7,21 +7,21 @@ class SlackbotsController < ApplicationController
   def event
     @body = JSON.parse(request.body.read)
     # if @body['event']['type'] == 'message' && @body['event']['text'].include?('<@US68T0M1Q>')
-
       case @body['type']
       when 'url_verification'
         render json: @body
       when 'event_callback'
+
       render status: 200, json: { status: 200 }
-      Slack.configure do |config|
-        config.token = ENV['SLACK_OAUTH_ACCESS_TOKEN']
-      end
-      client = Slack::Web::Client.new
-      client.chat_postMessage(
-          as_user: 'true',
-          channel: '#zozo_scraping',
-          text: 'suc'
-      )
+        Slack.configure do |config|
+          config.token = ENV['SLACK_OAUTH_ACCESS_TOKEN']
+        end
+        client = Slack::Web::Client.new
+        client.chat_postMessage(
+            as_user: 'true',
+            channel: '#zozo_scraping',
+            text: 'suc'
+        )
         # ZozoScraping.fetch_wear_page("https://wear.jp/men-category/tops/knit-sweater/")
       end
     # else
